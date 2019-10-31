04b0a871d7407aaff0d51f483d3c071d19dd963a
Abdus-Samad Titus <abdussamadtitus@gmail.com>
2019-10-31 16:21:29 +0200

    fix forgot pass

--- new_pass.php
+++ new_pass.php
@@ -7,10 +7,10 @@
     </head>
     <body>
         <div class="signin">
-            <form action="verify.php" method="POST">
+            <form action="new.php" method="POST">
                 <h2 style="color: white">Reset Password</h2>
-                <input type ="email" name="conf_email" placeholder="Please enter your email">
-                <input type="submit" name="send" placeholder="send link">
+                <input type ="password" name="new_pass" placeholder="Please enter your new password">
+                <input type="submit" name="change" placeholder="send link">
                 <br>
             </form>
         </div>
--- reset_password.php
+++ reset_password.php
@@ -1,3 +1,94 @@
 <?php
+    //enter email
+    //verify email
+    //if verified send link to reset password
+    //update password in database.
+
+    session_start();
+        include("forgot_password.php");
+        include("otp.php");
+        include("new_pass.php");
     
+        include_once 'connect.php';
+    
+        if(isset($_POST['send']))
+        {
+            $c_email = $_POST['conf_email'];
+    
+        //    if(empty($otp))
+        //    {
+        //        echo "<div class='error_message'>Please enter an OTP</div>";
+        //    }
+           //else
+           //{
+                $stmt = $conn->prepare("SELECT * FROM users WHERE email = '$c_email'");
+                $stmt->execute();
+                $result = $stmt->fetch();
+                $_new_otp = rand(10000, 99000);
+                //update otp to new one
+                $stmt = $conn->prepare("SELECT * FROM users WHERE otp = '$new_otp'");
+                $stmt->execute();
+                $query =$conn->prepare("UPDATE users SET otp = $new_otp WHERE email = $c_email");
+                $query->execute();
+                if ($result) 
+                {
+
+                    //////////////////conf email
+
+                $to = $c_email;
+                $subject = "Verify Email";
+                $message = "<h1><a href ='http://localhost:8080/camagru/otp.php?link=$new_otp'>click here</a></h1>
+                <p></br></br></br></p>
+                <p>Your otp: $new_otp</p></br>
+                <p>link: 'http://localhost:8080/camagru/otp.php?link=$new_otp'</p>";
+                $from = "abdussamadtitus@gmail.com";
+                $headers = "MIME-Version: 1.0" . "\n";
+                $headers .= "Content-type:text/html;charset=iso-8859-1" . "\n";
+                $headers .= "From: $from" . "\n";
+
+                mail($to,$subject,$message,$headers);
+
+                echo "We have just sent you an email with your verification link.";
+                header("Location: http://localhost:8080/camagru/otp.php");
+                /////////////////////////////////////////verify new otp
+                if(isset($_POST['verify']))
+                {
+                    $otp_verify = $_POST['otp'];
+            
+                //    if(empty($otp))
+                //    {
+                //        echo "<div class='error_message'>Please enter an OTP</div>";
+                //    }
+                //else
+                //{
+                        $stmt = $conn->prepare("SELECT * FROM users WHERE otp = '$otp_verify'");
+                        $stmt->execute();
+                        $result = $stmt->fetch();
+                        if ($result) 
+                        {
+                            header("Location: http://localhost:8080/camagru/new_pass.php");
+
+                            if(isset($_POST['change']))
+                            {
+                                $n_pass = $_POST['new_pass'];
+                                $query =$conn->prepare("UPDATE users SET user_password = $n_pass WHERE email = $c_email");
+                                $query->execute();
+                                echo "your password has been updated";
+                                header("Location: http://localhost:8080/camagru/index.php");
+                                
+                            }
+                        }
+                        else
+                        {
+                            echo "<div class='error_message'>Incorrect otp</div>";
+                        }
+                    }
+
+                
+                }
+                else
+                {
+                    echo "<div class='error_message'>Incorrect otp</div>";
+                }
+            }
 ?>
