fc3f7a20f1b794be72c7b3e50dbb1285e4f96a42
Abdus-Samad Titus <abdussamadtitus@gmail.com>
2019-11-14 16:01:47 +0200

    fix like and commnt

--- private_gallery.php
+++ private_gallery.php
@@ -1,14 +1,15 @@
 <?php
+    session_start();
     include_once ('config/database.php');
 
     echo "test";
     $result = $conn->prepare("SELECT * FROM images");
     $result->execute();
 
-    while ($data = $result->fetch(PDO::FETCH_ASSOC)){
-        // do something awesome with row
-      //  echo "<img src=".$data["image_name"].">";
+    while ($data = $result->fetch(PDO::FETCH_ASSOC))
+    {
         echo "<h1>{$data['image_name']}</h1>";
-       echo "<img src='".$data['image_path']."' width='400px' height='400px'>";
-     } 
+        echo "<img src='".$data['image_path']."' width='300px' height='300px'>";
+        echo"<div><button type='button' action='private_gallery.php' method='POST' name='like'>Like</button></div>";
+    }
 ?>
